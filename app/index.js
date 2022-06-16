73a4be542eea168292192fe39e8761ad30d69f5d
Andrii Vorochuk AQA <99738836+AndriiAQA@users.noreply.github.com>
2022-06-16 10:13:39 +0300

    add Lesson 10

--- app/index.js
+++ app/index.js
@@ -2,69 +2,51 @@
 
 // console.log(`Hello Aunomation team !`);
 
-let userName = "Artem";
-let userSureName = "Vepryk";
-
-const getFullName = (name, surename) => {
-  return `${name} ${surename}`
+let user = {
+  
+  isActive: false,
+  balance: '$2,474.46',
+  age: 30,
+  eyeColor: 'blue',
+  name: 'Tameka Maxwell',
+  gender: 'female',
+  company: 'ENOMEN',
+  email: 'tamekamaxwell@enomen.com',
+  phone: '+1 (902) 557-3898',
+  tags: [ 'aliquip', 'anim', 'exercitation', 'non',  'Barber Hicks' ],
+  friends: [
+    { id: 0, name: 'Barber Hicks' },
+    { id: 1, name: 'Santana Cruz' },
+    { id: 2, name: 'Leola Cabrera' }
+  ]
 }
 
+const newUser = {
+  guid: user.guid,
+  isActive: user.isActive,
+  name: user.name,
+  friends: [user.friends[0]]
+};
 
-function somethingPrint(name = "Andrii", sureName = "Vorochuk", age) {
-  let str = "Hello Aunomation team "
-  console.log(`${str} and ${getFullName(name, sureName)} !`)
-}
-// somethingPrint();
-
-// const funcEmpty = somethingPrint()
-// somethingPrint(userName, userSureName);
-// somethingPrint("Olersandr", "Chekusov")
-
-// ((name, surename) => {
-//   let str = "Hello Aunomation team "
-//   console.log(`${str} and ${getFullName(name, surename)} !`)
-// })("Artem", "Vepryk---------");
-
-(() => { })();
-
-function printFirst(a, b) {
-
-  return () => { }
+const newUser2 = {
+  id: user.id
 }
 
+// friends[0] = { id: 555, name: "Hello World!"}
 
-let vvv = {
-  name: "asdasd",
-  arr1: {
-    asdas: {
-      a: 123,
-      b: 456
-    }
-  }
+// let [ firstElem, secondElem ] = tags; /// TODO 
+// let lastEl = tags[tags.length - 1];
+
+// tags[1] = "Leo";
+// tagsRest[1] = "newLeo";
+
+// const user3 = (obj) =>{ return { guid:id = "sdfnsldvnsdlkvnsdlkn", isActive, gender, friends, tags, fullName = "AllTeam" } = obj };
+
+const { guid:id = "sdfnsldvnsdlkvnsdlkn", isActive, gender, friends: [,,{name: frName, frfullName = "default"}], tags, fullName = "AllTeam" } = user
+
+const user3 = {
+  id, isActive, gender, frName, frfullName, fullName
 }
 
-const a = 4;
-let str;
-
-switch (true) {
-  case a < 0:  // if (x === 'value1')
-    str = "lower 0"
-    break
-
-  case a > 0 && a < 10:  // if (x === 'value2')
-    str = "from 0 to 10"
-    break
-
-  case a > 10:  // if (;x === 'value2')
-    str = "Upper 10";
-    break;
-
-  default:
-    str = "Default";
-    break;
-
-
-}
-
-console.log(str)
+console.log("user3 :>> ", user3);
 
